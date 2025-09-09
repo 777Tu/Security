a255f920a0bd79e91befc769f402a0d456103e66
Scott <191410593+777Tu@users.noreply.github.com>
2025-09-09 18:43:54 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, make_response,render_template
+
+app = Flask(__name__)
+
+@app.after_request
+def add_security_headers(response):
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    response.headers["Referrer-Policy"] = "no-referrer"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    return response
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
