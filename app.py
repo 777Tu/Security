bfac43a2eaebae0306591af4e3bc4c9bc1e5ae76
Scott <191410593+777Tu@users.noreply.github.com>
2025-09-09 19:43:24 +0100

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def add_security_headers(response):
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
     response.headers["Referrer-Policy"] = "no-referrer"
     response.headers["X-Content-Type-Options"] = "nosniff"
-    response.headers["X-MySecurity"] = "Secured by Tuscott 🤓"
+    response.headers["X-MySecurity"] = "Secured by Tuscott "
     return response
 
 @app.route("/")
